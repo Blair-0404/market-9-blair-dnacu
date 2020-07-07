2e3dc22e2be37c7f60f07b93b678cf45537fbe08
blair B <blair441035@gmail.com>
2020-07-08 05:38:01 +0900

    가입페이지-아이디 인증구현

--- pages/signUp/signUp.js
+++ pages/signUp/signUp.js
@@ -1,42 +1,50 @@
-const form = document.querySelector('form')
+// const form = document.querySelector('form')
 const id = document.querySelector('.id')
-const label = document.querySelector('label')
-
 const password = document.querySelector('.password')
 const password2 = document.querySelector('.password2')
 const emailBtn = document.querySelector('.email-bottom')
 const emailSelect = document.querySelector('#email-select')
 const name = document.querySelector('.name')
 const phone = document.querySelector('.phone')
-
 const phoneAuthBtn = document.querySelector('.phone-auth-button')
 const checkSelectInfo = document.querySelector('.check-select-info')
 
-
-form.addEventListener('onChange', (e) => {
+// id
+id.addEventListener('focusout', (e) => {
     e.preventDefault();
-    checkInputs();
+    isValidatedId();
 })
 
-
-function checkInputs() {
+function isValidatedId() {
     const idValue = id.value.trim();
+    const generalRegExpId = /^[0-9a-z-_]+$/;
+    const isGeneralRegEx = generalRegExpId.test(idValue);
+    const label = document.querySelector('.id-msg');
+    const lengthErrorMsg = "아이디는 4글자 이상 20글자 이하로 사용 가능합니다";
+    const generalErrorMsg = "아이디는 소문자와 숫자만 사용 가능합니다";
+    const validateMsg = "입력하신 아이디로 사용 가능합니다";
 
-    if ((idValue.length > 4 || idValue.length < 20)) {
-        // id.classList.add("error");
-        let label = document.createElement('label');
-        label.innerText = '입력하신 아이디로 사용이 가능합니다.'
-
+    // validation logic
+    if (idValue.length < 4 || idValue.length > 20) {
+        id.classList.add("invalid");
+        label.textContent = lengthErrorMsg;
+    } else if (!isGeneralRegEx) {
+        id.classList.add("invalid");
+        label.textContent = generalErrorMsg;
+    } else {
+        id.classList.remove("invalid");
+        label.textContent  = validateMsg;
     }
 }
 
+// name
 
-// let newDiv = document.createElement('div');
-// // Add class name
-// newDiv.className= 'hello';
-//
-// // Add id name
-// newDiv.id= 'hello1';
-//
-// // Add attribute - 태그 속성 추가
-// newDiv.setAttribute('title', 'Hello Div');
+name.addEventListener('focusout', (e) => {
+    e.preventDefault();
+    isValidatedId();
+})
+
+
+function isValidatedPassword() {
+
+}
